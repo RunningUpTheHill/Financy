09268fe2bc9f52c4f911b817340a6f6850f5288a
jameslinejeudy <jjeudy@ufl.edu>
2024-04-22 14:32:23 -0400

    shopping landing

---
+++ client/src/pages/categories/Shopping.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+const ShoppingStyle = {
+    alignItems: 'center', // center horizontally
+    display: 'inline-block',
+    width: '900px',
+    height: '525px',
+    backgroundColor: '#ffffff',
+    borderRadius: '25px',
+    paddingLeft: '30px',
+    paddingRight: '30px',
+    
+  };
+  
+  const textStyle = {
+    color: 'black',
+    fontSize: '25px',
+    fontFamily: 'madimi one', // Corrected the font name to 'Arial'
+    textAlign: 'left',
+  };
+  
+  const paragraphStyle = {
+      borderBottom: '2px solid black', // Adds a border to the bottom of each paragraph
+      paddingBottom: '8px',
+      paddingTop: '2px',
+      marginBottom: '4px', // Adds
+  };
+
+
+  export default function Shopping() {
+    return (
+      <>
+      <div style={ShoppingStyle}>
+        <h1 style={{textStyle, textAlign: 'center', fontSize: '30px' }}>Shopping</h1>
+        <p style={{ ...textStyle, ...paragraphStyle }}>Checking</p>     
+        <p style={{ ...textStyle, ...paragraphStyle }}>Savings</p>   
+        <p style={{ ...textStyle}}>Credit</p>
+      </div>
+        </>
+    );
+  }
+    
